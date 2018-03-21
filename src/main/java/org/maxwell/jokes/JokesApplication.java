990e774aeb4cfbf7a99a4e1ac5c186579c46c339
Sapna Deraje Radhakrishna <sapna.dr@gmail.com>
2018-03-21 11:22:50 -0700

    added xml based configuration

--- src/main/java/org/maxwell/jokes/JokesApplication.java
+++ src/main/java/org/maxwell/jokes/JokesApplication.java
@@ -2,8 +2,10 @@ package org.maxwell.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:jokes-config.xml")
 public class JokesApplication {
 	public static void main(String[] args) {	
 		SpringApplication.run(JokesApplication.class, args);
